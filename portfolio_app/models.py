c715a76ac088cc8f4433058a8e70acf1b3ae4bb6
Suresh-web-dev <dssuresh68@gmail.com>
2025-07-25 11:32:18 -0700

    some changes

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
     image = models.ImageField(upload_to='media/projects/')
     github_link = models.URLField(blank=True)
     demo_link = models.URLField(blank=True)
