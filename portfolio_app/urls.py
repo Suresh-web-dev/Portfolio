e58b3ae73d46f94d5372db29ef99024e77667b9b
Suresh-web-dev <dssuresh68@gmail.com>
2025-08-01 14:06:43 -0700

    Add home page URL and template

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from portfolio_app.views import home,about,projects,skills,education,contact
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',home, name='home'),
     path("home/",home),
     path("about/",about),
     path('projects/',projects),
