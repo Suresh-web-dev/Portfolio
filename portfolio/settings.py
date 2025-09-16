c4c07f5453b81ea3eb9700ad9f9e10f663bf11bb
Suresh-web-dev <dssuresh68@gmail.com>
2025-09-16 11:05:53 -0700

    deploye vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-+u+4_-k$6#825*m(e10v2y77m16hja1hpc8y_h7_h3oy7xgrd2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Add this line below STATIC_URL
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from portfolio_app.views import index
 
 urlpatterns = [
+    path('',index),
     path('index/',index),
 ]
